fb67adecbed90fe21a3e2f2627805576005e1efc
Acner Mendez Feliz <acnermendezfeliz@gmail.com>
2023-06-10 04:29:07 +0200

    "Segundo commit de proyecto He realizado una paginaa principal para la barberia desde la que los usuarios tendran acceso a registrarse en la barberia y ha hacceder una ves tenga un usuario"

---
+++ Login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylos.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="Tit">
+        <h1>Barberia</h1>
+    </div>
+    
+    <div class="banner">
+        <div class="capa"></div>
+        <div class="info">
+            <h1>Barberia</h1>
+
+            <p> Bienvenidos a nuestra barberias, Estamus a su dispocición </p>
+                <a href="#">Registrate</a>
+                <a href="#">Entrar</a>
+        </div>
+    
+</div>
+</body>
+</html>
